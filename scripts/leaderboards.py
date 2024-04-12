b26ed0e14da33f303b15ca522b608f80bc1a3f19
imogensole <imogensole02@gmail.com>
2024-04-12 06:12:38 +0100

    Update leaderboards.py adding comments

--- scripts/leaderboards.py
+++ scripts/leaderboards.py
@@ -4,16 +4,20 @@ import pandas as pd
 data = pd.read_csv("data/player_metrics.csv")
 
 while True:
+    # Ask user to choose a metric for the leaderboard 
     metric = input("How would you like the players ranked? Choose from 'Top Speed', 'Total Distance', 'Distance in Zone 5' or 'Time With Ball' ")
 
     if metric in data.columns:
+        # Sort leaderboard based on the chosen metric 
         leaderboard = data.sort_values(by=metric, ascending=False)[['participation_id', metric]]
 
         print("Leaderboard based on", metric)
         print(leaderboard)
     else:
+        # Show error message if chosen metric is invalid 
         print("Sorry, the entered metric is not valid.")
 
+    # Ask user if they would like to view another leaderboard
     continues = input("Would you like to view another leaderboard? (yes/no)")
 
     if continues != 'yes': 
